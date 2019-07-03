0a3bedec749a96b770e6ffb4b65419d6ccddc330
nikaku <qurdadzenika@gmail.com>
2019-07-03 16:19:41 +0400

    Add project files.

---
+++ AirPortsCancelRevaluationExchangeRates/CancelByDate.b1f.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using SAPbobsCOM;
+using SAPbouiCOM;
+using SAPbouiCOM.Framework;
+using Application = SAPbouiCOM.Framework.Application;
+
+namespace AirPortsCancelRevaluationExchangeRates
+{
+    [FormAttribute("AirPortsCancelRevaluationExchangeRates.CancelByDate", "CancelByDate.b1f")]
+    class CancelByDate : UserFormBase
+    {
+        public CancelByDate()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_0").Specific));
+            this.EditText0 = ((SAPbouiCOM.EditText)(this.GetItem("Item_1").Specific));
+            this.StaticText1 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_2").Specific));
+            this.EditText1 = ((SAPbouiCOM.EditText)(this.GetItem("Item_3").Specific));
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_4").Specific));
+            this.Button0.PressedAfter += new SAPbouiCOM._IButtonEvents_PressedAfterEventHandler(this.Button0_PressedAfter);
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.StaticText StaticText0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private SAPbouiCOM.EditText EditText0;
+        private SAPbouiCOM.StaticText StaticText1;
+        private SAPbouiCOM.EditText EditText1;
+        private SAPbouiCOM.Button Button0;
+
+        private void Button0_PressedAfter(object sboObject, SAPbouiCOM.SBOItemEventArg pVal)
+        {
+
+            if (!DiManager.Company.InTransaction)
+            {
+                DiManager.Company.StartTransaction();
+            }
+
+            if (string.IsNullOrWhiteSpace(EditText0.Value) || string.IsNullOrWhiteSpace(EditText0.Value))
+            {
+                Application.SBO_Application.SetStatusBarMessage("შეავსეთ თარიღები",
+                    BoMessageTime.bmt_Short, true);
+                return;
+            }
+
+            Recordset recSet =
+                (Recordset)DiManager.Company.GetBusinessObject(BoObjectTypes
+                    .BoRecordset);
+            string query =
+                $"select TransId from JDT1 where TransId in (SELECT TransId FROM JDT1 WHERE RefDate IN(SELECT   MAX(RefDate) FROM     JDT1 GROUP BY MONTH(RefDate), YEAR(RefDate)) AND(Account = '8180' OR Account = '8280')  AND(ContraAct in (SELECT CardCode FROM OCRD)) AND(RefDate >= '{DateTime.ParseExact(EditText0.Value, "yyyyMMdd", CultureInfo.InvariantCulture):s}' AND RefDate <= '{DateTime.ParseExact(EditText1.Value, "yyyyMMdd", CultureInfo.InvariantCulture):s}')) AND Ref3Line  LIKE N'%RC%' OR Ref3Line  LIKE N'%БО%' OR Ref3Line LIKE N'%ПР%' OR Ref3Line LIKE N'%РС%'";
+            recSet.DoQuery(DiManager.QueryHanaTransalte(query));
+
+            while (!recSet.EoF)
+            {
+                int transId = int.Parse(recSet.Fields.Item("TransId").Value.ToString());
+                JournalEntries journalEntry =
+                    (JournalEntries)DiManager.Company.GetBusinessObject(BoObjectTypes.oJournalEntries);
+                journalEntry.GetByKey(transId);
+
+                bool isDpm = false;
+
+                for (int i = 0; i < journalEntry.Lines.Count; i++)
+                {
+                    journalEntry.Lines.SetCurrentLine(i);
+                    if (journalEntry.Lines.AdditionalReference.Contains("РС"))
+                    {
+                        isDpm = true;
+                    }
+                }
+
+                int res;
+
+                if (isDpm)
+                {
+                    journalEntry.UseAutoStorno = BoYesNoEnum.tYES;
+                    journalEntry.StornoDate = journalEntry.TaxDate;
+                    res = journalEntry.Update();
+                }
+                else
+                {
+                    journalEntry.UseAutoStorno = BoYesNoEnum.tYES;
+                    journalEntry.StornoDate = new DateTime(2019, 01, 01);
+                    res = journalEntry.Update();
+                }
+
+                if (res != 0)
+                {
+                    Application.SBO_Application.SetStatusBarMessage($"journal Entry : {journalEntry.Number}  {DiManager.Company.GetLastErrorDescription()}",
+                        BoMessageTime.bmt_Short, true);
+                    if (DiManager.Company.InTransaction)
+                    {
+                        DiManager.Company.EndTransaction(BoWfTransOpt.wf_RollBack);
+                    }
+                }
+                recSet.MoveNext();
+            }
+            if (DiManager.Company.InTransaction)
+            {
+                DiManager.Company.EndTransaction(BoWfTransOpt.wf_Commit);
+            }
+
+        }
+    }
+}
---
+++ AirPortsCancelRevaluationExchangeRates/Form1.b1f.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using SAPbobsCOM;
+using SAPbouiCOM;
+using SAPbouiCOM.Framework;
+using Application = SAPbouiCOM.Framework.Application;
+
+namespace AirPortsCancelRevaluationExchangeRates
+{
+    [FormAttribute("AirPortsCancelRevaluationExchangeRates.Form1", "Form1.b1f")]
+    class Form1 : UserFormBase
+    {
+        public Form1()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_0").Specific));
+            //this.Button0.PressedAfter += new SAPbouiCOM._IButtonEvents_PressedAfterEventHandler(this.Button0_PressedAfter);
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.Button Button0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        //private void Button0_PressedAfter(object sboObject, SAPbouiCOM.SBOItemEventArg pVal)
+        //{
+        //    Recordset recSet =
+        //        (Recordset)DiManager.Company.GetBusinessObject(BoObjectTypes
+        //            .BoRecordset);
+        //    recSet.DoQuery(DiManager.QueryHanaTransalte($" select TransId from JDT1 where TransId in (SELECT TransId FROM JDT1 WHERE RefDate IN(SELECT   MAX(RefDate) FROM     JDT1 GROUP BY MONTH(RefDate), YEAR(RefDate)) AND(Account = '8180' OR Account = '8280')  AND(ContraAct in (SELECT CardCode FROM OCRD)) AND(RefDate >= '2017-10-31 00:00:00.000')) AND Ref3Line  LIKE N'%RC%' OR Ref3Line  LIKE N'%БО%' OR Ref3Line LIKE N'%ПР%' OR Ref3Line LIKE N'%РС%'"));
+
+        //    while (recSet.EoF)
+        //    {
+        //        int transId = int.Parse(recSet.Fields.Item("TransId").Value.ToString());
+        //        JournalEntries journalEntry =
+        //            (JournalEntries)DiManager.Company.GetBusinessObject(BoObjectTypes.oJournalEntries);
+        //        journalEntry.GetByKey(transId);
+
+        //        bool isDpm = false;
+
+        //        for (int i = 0; i < journalEntry.Lines.Count; i++)
+        //        {
+        //            journalEntry.SetCurrentLine(i);
+        //            if (journalEntry.Lines.AdditionalReference.Contains("РС"))
+        //            {
+        //                isDpm = true;
+        //            }
+        //        }
+
+        //        int res;
+
+        //        if (isDpm)
+        //        {
+        //            journalEntry.UseAutoStorno = BoYesNoEnum.tYES;
+        //            journalEntry.StornoDate = journalEntry.TaxDate;
+        //            res = journalEntry.Update();
+        //        }
+        //        else
+        //        {
+        //            journalEntry.UseAutoStorno = BoYesNoEnum.tYES;
+        //            journalEntry.StornoDate = new DateTime(2019, 01, 01);
+        //            res = journalEntry.Update();
+        //        }
+
+        //        if (res != 0)
+        //        {
+        //            Application.SBO_Application.SetStatusBarMessage($"journal Entry : {journalEntry.Number}  {DiManager.Company.GetLastErrorDescription()}",
+        //                BoMessageTime.bmt_Short, true);
+        //        }
+
+        //        recSet.MoveNext();
+        //    }
+
+        //}
+    }
+}
